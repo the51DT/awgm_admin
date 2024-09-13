0ae6b65e9fb7f00af878fac29c5f132adc6e4be9
100minkyung <mkbaek@the-51.com>
2024-09-13 11:15:55 +0900

    AppBanner02 추가

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import AppMenu1 from "@/views/AppMenu1.vue";
 import AppMenu2 from "@/views/AppMenu2.vue";
 import AppLogin from "@/views/AppLogin.vue";
 import AppBaner01 from "@/views/AppBanner01.vue";
+import AppBaner02 from "@/views/AppBanner02.vue";
 import AppNotice from "@/views/AppNotice.vue";
 
 const routes = [
@@ -12,7 +13,7 @@ const routes = [
   { path: "/AppMenu2", component: AppMenu2 },
   { path: "/AppMenu21", component: AppNotice },
   { path: "/AppMenu3", component: AppBaner01 },
-  { path: "/AppMenu3", component: AppBaner01 },
+  { path: "/AppBaner02", component: AppBaner02 },
   { path: "/Applogin", component: AppLogin, meta: { layout: "none" } },
 ];
 
