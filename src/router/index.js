d33aba7ebce1924abb9c56958826869531d12cff
YB <ybjeong@the-51.com>
2024-09-20 15:34:18 +0900

    t

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import AppMenu1 from "@/views/AppMenu1.vue";
 import AppMenu2 from "@/views/AppMenu2.vue";
-import AppLogin from "@/views/AppLogin.vue";
+import GutAd00001 from "@/views/GutAd00001.vue";
 import AppBanner01 from "@/views/AppBanner01.vue";
 import AppBanner02 from "@/views/AppBanner02.vue";
 import AppChallenge02 from "@/views/AppChallenge02.vue";
@@ -16,9 +16,13 @@ const routes = [
   { path: "/AppMenu21", component: AppNotice },
   { path: "/AppMenu3", component: AppBanner01 },
   { path: "/AppBanner02", component: AppBanner02, name: "AppBanner02" },
-  { path: "/AppChallenge02", component: AppChallenge02, name: "AppChallenge02" },
+  {
+    path: "/AppChallenge02",
+    component: AppChallenge02,
+    name: "AppChallenge02",
+  },
   { path: "/AppCS04", component: AppCS04, name: "AppCS04" },
-  { path: "/Applogin", component: AppLogin, meta: { layout: "none" } },
+  { path: "/Applogin", component: GutAd00001, meta: { layout: "none" } },
 ];
 
 const router = createRouter({
