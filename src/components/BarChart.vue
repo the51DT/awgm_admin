f1f030a0c657d78d035fd4f0174d21397641703e
YB <ybjeong@the-51.com>
2024-09-23 14:58:10 +0900

    ex chart

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -29,8 +29,30 @@ export default {
   data() {
     return {
       chartData: {
-        labels: ["January", "February", "March"],
-        datasets: [{ data: [40, 20, 12] }],
+        labels: [
+          "12월 01일",
+          "12월 02일",
+          "12월 03일",
+          "12월 04일",
+          "12월 05일",
+          "12월 06일",
+          "12월 07일",
+          "12월 08일",
+          "12월 09일",
+          "12월 10일",
+        ],
+        datasets: [
+          {
+            label: "가입",
+            backgroundColor: "blue",
+            data: [300, 500, 800, 900, 950, 1000, 1200, 1300, 1500, 2400],
+          },
+          {
+            label: "탈퇴",
+            backgroundColor: "#f87979",
+            data: [10, 10, 10, 10, 10, 35, 318, 321, 324, 327],
+          },
+        ],
       },
       chartOptions: {
         responsive: true,
