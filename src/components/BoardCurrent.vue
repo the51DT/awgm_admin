8cb68eccf630cead40007e26b6280f9de0974638
YB <ybjeong@the-51.com>
2024-11-18 14:30:10 +0900

    boardcurrent

---
+++ src/components/BoardCurrent.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="board-info-current">
+    <span>전체<strong class="board-info-current-total">44</strong>건</span>
+    <span>현재페이지<strong class="board-info-current-state">1/5</strong></span>
+  </div>
+</template>
