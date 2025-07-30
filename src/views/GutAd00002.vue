5d0a0371090da8dd1ffd1150d5187ad7e92fb837
YB <ybjeong@the-51.com>
2025-07-30 10:27:48 +0900

    GutAd00002

--- src/views/GutAd00002.vue
+++ src/views/GutAd00002.vue
@@ -4,7 +4,7 @@
       <v-card class="custom" flat>
         <v-card-title class="card-title">DASHBOARD</v-card-title>
         <div class="scroll-area">
-          <v-sheet class="mt-4 pa-8 dash" rounded="lg">
+          <v-sheet class="mt-4 pl-8 pr-8 dash" rounded="lg">
             <v-row class="input-row" align="center">
               <v-col cols="1"></v-col>
               <v-col cols="7">
@@ -15,7 +15,7 @@
               </v-col>
             </v-row>
           </v-sheet>
-          <v-sheet class="mt-4 pa-8 dash" rounded="lg">
+          <v-sheet class="mt-4 pl-8 pr-8 dash" rounded="lg">
             <v-row class="input-row" align="center">
               <v-col cols="1"></v-col>
               <v-col cols="7">
@@ -26,7 +26,7 @@
               </v-col>
             </v-row>
           </v-sheet>
-          <v-sheet class="mt-4 pa-8 dash" rounded="lg">
+          <v-sheet class="mt-4 pl-8 pr-8 dash" rounded="lg">
             <v-row class="input-row" align="center">
               <v-col cols="1"></v-col>
               <v-col cols="7">
@@ -37,11 +37,33 @@
               </v-col>
             </v-row>
           </v-sheet>
-          <v-sheet class="mt-4 pa-8 dash" rounded="lg">
+          <v-sheet class="mt-4 pl-8 pr-8 dash" rounded="lg">
             <v-row class="input-row" align="center">
               <v-col cols="1"></v-col>
               <v-col cols="7">
                 <h4>오늘 미션인증 수</h4>
+              </v-col>
+              <v-col cols="4" align="right">
+                <span class="red">30,000 회</span>
+              </v-col>
+            </v-row>
+          </v-sheet>
+          <v-sheet class="mt-4 pl-8 pr-8 dash" rounded="lg">
+            <v-row class="input-row" align="center">
+              <v-col cols="1"></v-col>
+              <v-col cols="7">
+                <h4>오늘1 미션인증 수</h4>
+              </v-col>
+              <v-col cols="4" align="right">
+                <span class="red">30,000 회</span>
+              </v-col>
+            </v-row>
+          </v-sheet>
+          <v-sheet class="mt-4 pl-8 pr-8 dash" rounded="lg">
+            <v-row class="input-row" align="center">
+              <v-col cols="1"></v-col>
+              <v-col cols="7">
+                <h4>오늘2 미션인증 수</h4>
               </v-col>
               <v-col cols="4" align="right">
                 <span class="red">30,000 회</span>
